57dc7ba1844bedbbaaf6b96b073ec3bc0aa17575
Lê Thành Đạt <datthichlaptrinh@gmail.com>
2024-06-30 14:33:59 +0700

    Basket Microservices with Vertical Slice Architecture and CQRS

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,32 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+//Data Services
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+//Cross-Cutting Services
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+// Configure the HTTP request pipeline.
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+//app.UseHealthChecks("/health",
+//    new HealthCheckOptions
+//    {
+//        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+//    });
 app.Run();
