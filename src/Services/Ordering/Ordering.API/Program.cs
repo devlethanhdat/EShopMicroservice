084c95c4f6af5d0917107bd826124296507aa839
Lê Thành Đạt <datthichlaptrinh@gmail.com>
2024-07-12 16:20:03 +0700

    Ordering Microservices with DDD, CQRS and Clean Architecture

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,15 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services
+    .AddApplicationServices(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(builder.Configuration);
+var app = builder.Build();
+
+
+
+app.Run();
